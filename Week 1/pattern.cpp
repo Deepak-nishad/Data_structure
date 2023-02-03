f73662c265463f1658a06b658b0cec0466bde23a
Deepak-nishad <deepakkumar62559@gmail.com>
2023-02-03 12:22:20 +0530

    Initial commit

---
+++ Week 1/pattern.cpp
@@ -0,0 +1,41 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    // Solid rectangle
+    // for(int i=0; i<n;i++)
+    // {
+    //     for(int j=0;j<n;j++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+     for(int i=0; i<n;i++)
+    {
+        if(i==0 || i==n-1)
+        {
+           for(int j=0;j<n;j++)
+           {
+            cout<<"* ";
+           }
+           
+        }
+
+        else{
+         cout<<"* ";
+         for(int j=0;j<n-2;j++)
+           {
+            cout<<" ";
+           }
+           cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+}
